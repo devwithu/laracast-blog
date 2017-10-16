6c3001a02f62d74e0f21226fdc38cec41b9e72c9
DaeJin Jeong <devwithu@naver.com>
2017-10-17 08:36:54 +0900

    Episode 28 From Request

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-use App\Mail\Welcome;
+
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
 {
@@ -12,27 +11,9 @@ class RegistrationController extends Controller
     {
         return view('registration.create');
     }
-    public function store()
+    public function store(RegistrationForm $form)
     {
-
-        $this->validate(request(),[
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|confirmed',
-        ]);
-
-
-        //$user = User::create(request(['name', 'email', 'passwrod']));
-        $user = User::create([
-            'name' => request('name'),
-            'email' => request('email'),
-            'password' => bcrypt(request('password'))
-        ]);
-
-
-        auth()->login($user);
-
-        \Mail::to($user)->send(new Welcome($user));
+        $form->persist();
 
         return redirect()->home();
     }
