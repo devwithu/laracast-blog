aba3b0172f5152803907e60069636f19685431b6
DaeJin Jeong <devwithu@naver.com>
2017-10-18 08:36:40 +0900

    Episode 31 Sorting Posts by tags

--- app/Tag.php
+++ app/Tag.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-       public function posts()
+    public function posts()
     {
         return $this->belongsToMany(Post::class);
+    }
+
+    public function getRouteKeyName()
+    {
+    	return 'name';
     }
 }
